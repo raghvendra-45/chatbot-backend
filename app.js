fd6118dc9bc40b4d872a83ea7355abeae409e12a
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-12-17 18:39:14 +0530

    second commit

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
-app.use(bodyParser.json()); // Parse incoming JSON requests
+app.use(bodyParser.json());
 
-// Gemini API details
-const API_KEY = process.env.API_KEY; // Replace with your actual API key
+const API_KEY = process.env.API_KEY;
 const API_URL =
   "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +
   API_KEY;
 
-// Endpoint to handle chatbot requests
 app.post("/api/gemini", async (req, res) => {
   const { message } = req.body;
 
@@ -47,7 +44,6 @@ app.post("/api/gemini", async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
